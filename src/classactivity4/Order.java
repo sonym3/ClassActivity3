5a42609c178e4cc6bf7185207045bb5bbe5eb6f8
1894420 <1894420@E21037.CEGEPGIM.LCL>
2019-02-10 08:43:27 -0500

    create order class

---
+++ src/classactivity4/Order.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classactivity4;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1894420
+ */
+public class Order {
+    
+    Date date;
+    String status;
+    
+    public void calcSubTotal()
+    {
+        System.out.println("calcSubTotal");
+    }
+     public void calcTax()
+    {
+        System.out.println("calcTax");
+    }
+        public void calcTotal()
+    {
+        System.out.println("calcTotal");
+    }
+           public void calcTotalWeight()
+    {
+        System.out.println("calcTotalWeight");
+    }
+}
