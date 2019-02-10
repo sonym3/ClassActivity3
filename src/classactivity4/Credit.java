d5f9a2848e5e97b9dc1488a125e678c67945eac2
1894420 <1894420@E21037.CEGEPGIM.LCL>
2019-02-10 08:54:01 -0500

    create credit class

---
+++ src/classactivity4/Credit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classactivity4;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1894420
+ */
+public class Credit extends payment{
+    
+    String name, type;
+    Date expDate;
+    
+   
+}
