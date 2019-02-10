ddc7485a285342e12f2f6a830b13473f20026d58
1894461 <1894461@E21039.CEGEPGIM.LCL>
2019-02-10 08:59:33 -0500

    updated items

---
+++ src/item.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1894461
+ */
+public class item {
+     Double shoppingWeight;
+    String description ;
+    
+    public void getPriceForQuantity(){
+        System.out.println("this is a getPriceForQuantity function");
+    }
+    public void getTax(){
+        System.out.println("this is a getTax function");
+    }
+    
+    public void inStock(){
+        System.out.println("this is a inStock function");
+}
+}
+
